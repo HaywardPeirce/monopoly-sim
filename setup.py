7d59c057f53ba1602f0556159324886206065e68
HaywardPeirce <hpeirce@gmail.com>
2018-03-30 03:11:29 +0000

    HP: test out removing `include_package_data`

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='monopoly-sim',
-      version='0.0.4',
+      version='0.0.5',
       description='A monopoly game simulator',
       install_requires=['setuptools>=4.0.1'],
       url='https://github.com/HaywardPeirce/monopoly-sim',
       py_modules = ['monopoly'],
-      include_package_data=True,
+      #include_package_data=True,
       package_data={
         '': ['properties.json', 'places.json']
       }
